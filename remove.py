5e41abe713c8a71ed56d224c7fa520e30dd5e828
Cord Meados <meadosc@gmail.com>
2017-11-27 21:07:10 -0800

    relationship add and remove is working on cloud

--- remove.py
+++ remove.py
@@ -14,8 +14,21 @@ from exercise import Exercise
 
 class RemoveHandler(webapp2.RequestHandler):
 	def post(self, id=None):
-		w = ndb.Key(urlsafe=id).get()  #get the workout object from the database
-		##########################
-		###add in removal functionality
-		##########################
+		if id:
+			w = ndb.Key(urlsafe=id).get()  #get the workout object from the database
+			request_data = json.loads(self.request.body)
+			exerciseID = request_data["URL_ID"] #request must be json of form {"URL_ID":"exercise id here"}
+		
+			delFlag = -1
+			for i, exID in enumerate(w.exerciseIDs):			
+				if exID == exerciseID:
+					delFlag = i
+					self.response.write("match found" + str(i)) ###debug
+			if delFlag != -1:
+				w.exerciseIDs.pop(delFlag)
+				w.put()
+	
+	
+		
+		
 		
